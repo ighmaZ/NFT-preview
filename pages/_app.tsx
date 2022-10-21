23804295c62ba3570f21a271fb600d48c8a54799
ighmaz <ighmazcool@gmail.com>
2022-10-21 17:40:14 +0530

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from './components/Navbar'
+import Nft from './components/Nft'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+
+    <ChakraProvider>
+<Navbar/>
+<Nft/>
+      <Component {...pageProps} />
+
+    </ChakraProvider>
+
+  )
+ 
+
 }
 
 export default MyApp
