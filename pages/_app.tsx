fe05fc9f302e5abfb0544090aa47f50c214b2d76
ighmaz <ighmazcool@gmail.com>
2022-10-21 20:58:22 +0530

    added pics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,10 +15,33 @@ function MyApp({ Component, pageProps }: AppProps) {
 <Grid templateColumns='repeat(3, 2fr)' gap={2}>
 <Nft
  image={'https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149611030.jpg?w=2000'}
- />
+ 
+ title="Enotch"
+ price={260}/>
 <Nft
- image={'https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149611030.jpg?w=2000'}
- />
+ image={'https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149611048.jpg?w=740&t=st=1666365414~exp=1666366014~hmac=cea9e97a65027d089aa31ae932a5cc1d66683b3fcc9713377a9b062c40408ee6'}
+ title="Guy meraz"
+ price={300}/>
+<Nft
+ image={'https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149622024.jpg?w=740&t=st=1666365478~exp=1666366078~hmac=2c508cee1f6886aa5aae3dbd2e416fb6a708fc08af3721d9000a5d1a888299ba'}
+ title="Sambo cheif"
+ price={210}/>
+<Nft
+ image={'https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149611048.jpg?w=740&t=st=1666365414~exp=1666366014~hmac=cea9e97a65027d089aa31ae932a5cc1d66683b3fcc9713377a9b062c40408ee6'}
+ title="Abx Baken"
+ price={510}/>
+<Nft
+image={"https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149622021.jpg?w=740&t=st=1666365812~exp=1666366412~hmac=13229567974ee31b33cf9e52ce1a332086101bd0dfb5d2586fb5ca67cd7e6f90"}
+title="Tom Hosta"
+price={230}/>
+<Nft
+image={"https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149619508.jpg?w=740&t=st=1666365846~exp=1666366446~hmac=1a28238d5f05408d212c4d4222211bf45cc0aaa6321bfe3126185556749f68a3"}
+title="Besar Watson"
+price={312}
+/>
+
+
+ 
 
       <Component {...pageProps} />
       </Grid>
--- pages/components/Nft.tsx
+++ pages/components/Nft.tsx
@@ -1,35 +1,39 @@
-import { Badge, Box ,Text, HStack } from '@chakra-ui/react'
+import { Badge, Box ,Text, HStack,Button } from '@chakra-ui/react'
 import React from 'react'
-import Image from 'next/image'
+import {  StarIcon } from '@chakra-ui/icons'
  
 type NftProps = {
 
-//   price: number
+  price: number
   image: string
-//   title: string
+  title: string
 }
 
 
-const Nft = ({image}:NftProps) => {
+const Nft = ({price,image,title}:NftProps) => {
   return (
-   <Box w="300px" margin="50px"  rounded="20px" overflow="hidden" shadow="sm" borderWidth="8px" borderColor="purple.500" bg="blue.700"> 
+   <Box w="300px" margin="50px"  rounded="20px" overflow="hidden" shadow="sm" borderWidth="8px" borderColor="purple.400" bg="purple.100"> 
    <img src={image} />
 <Box p={10}>
 <Text 
  bgGradient="linear(to-l, #7928CA, #FF0080)"
   bgClip="text"
   fontSize="3xl"
-  fontWeight="extrabold">Enoch Citizen</Text>
+  fontWeight="extrabold">{title}</Text>
 <Badge variant='solid' rounded='full' px={2} marginRight="30px" >HOT DEAL</Badge>
 <Badge variant='solid' rounded='full' px={2}>AUCTION</Badge>
-<HStack>
+<HStack justifyContent="space-between">
     <Text
     marginTop="10px"
-    color="green.500"
+    color="green.300"
     fontSize="xl"
     fontWeight="extrabold"
-    >$250</Text>
+    >${price}</Text>
+   <StarIcon color="red.500" />
 </HStack>
+<Button 
+colorScheme ="purple"
+>Bid Now</Button>
 </Box>
    </Box>
   )
